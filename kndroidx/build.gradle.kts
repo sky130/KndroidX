5083aa45ece4d5d40b896c1c7e54635cc317247b
Sky233ml <sky233@sky233.ml>
2023-10-28 19:20:39 +0800

    For Jitpack

--- kndroidx/build.gradle.kts
+++ kndroidx/build.gradle.kts
@@ -1,16 +1,44 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
+
+val GROUP_ID = "com.github.KndroidX"
+val ARTIFACT_ID = "KndroidX"
+val VERSION = latestGitTag().ifEmpty { "0.1.0-alpha" }
+
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return  process.inputStream.bufferedReader().use {bufferedReader ->
+        bufferedReader.readText().trim()
+    }
+}
+
+
+publishing { // 发布配置
+    publications { // 发布的内容
+        register<MavenPublication>("release") { // 注册一个名字为 release 的发布内容
+            groupId = GROUP_ID
+            artifactId = ARTIFACT_ID
+            version = VERSION
+
+            afterEvaluate { // 在所有的配置都完成之后执行
+                // 从当前 module 的 release 包中发布
+                from(components["release"])
+            }
+        }
+    }
+}
+
 
 android {
     compileSdk = 33
     namespace = "http://schemas.android.com/apk/res/android"
     version = 1
     defaultConfig {
-        namespace = "io.github.sky130.kndroidx"
+        namespace = "io.github.kndroidx"
         minSdk = 21
-        targetSdk = 33
     }
 
     buildTypes {
