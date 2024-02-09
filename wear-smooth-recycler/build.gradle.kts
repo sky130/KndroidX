9e2095ac42db973f1b7e8dbc975d07fda25384fe
Sky233ml <sky233@sky233.ml>
2024-02-09 12:15:06 +0800

    新增类库

---
+++ wear-smooth-recycler/build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+publishing { // 发布配置
+    publications { // 发布的内容
+        register<MavenPublication>("release") { // 注册一个名字为 release 的发布内容
+            groupId = "com.github.kndroidx"
+            artifactId = "wear-smooth-recycler"
+            version =  latestGitTag().ifEmpty { "0.1.1-alpha" }
+
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
+}
+
+fun latestGitTag(): String {
+    val process = ProcessBuilder("git", "describe", "--tags", "--abbrev=0").start()
+    return process.inputStream.bufferedReader().use { bufferedReader ->
+        bufferedReader.readText().trim()
+    }
+}
+
+android {
+    namespace = "kndroidx.wear.recyclerview"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    api("androidx.core:core-ktx:1.12.0")
+    api("androidx.appcompat:appcompat:1.6.1")
+    api("androidx.wear:wear:1.4.0-alpha01")
+    implementation(project(":core"))
+}
