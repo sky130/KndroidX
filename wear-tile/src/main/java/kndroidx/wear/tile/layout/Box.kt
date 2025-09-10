3135fe1501db600a99d82c0ad9a0bd8f30e70b1d
Sky233 <sky233ml@qq.com>
2025-09-10 20:35:35 +0800

    Fix: 优化 `wear-tile` 模块
    - 完善 `Box` 布局

--- wear-tile/src/main/java/kndroidx/wear/tile/layout/Box.kt
+++ wear-tile/src/main/java/kndroidx/wear/tile/layout/Box.kt
@@ -6,8 +6,12 @@ import kndroidx.wear.tile.addLayoutElement
 
 fun Any.Box(
     modifier: ModifierWrapper? = null,
+    horizontalAlignment: Int? = null,
+    verticalAlignment : Int? = null,
     block: (LayoutElementBuilders.Box.Builder.() -> Unit)? = null,
 ) = LayoutElementBuilders.Box.Builder().apply {
+    horizontalAlignment?.let { setHorizontalAlignment(it) }
+    verticalAlignment?.let { setVerticalAlignment(it) }
     modifier?.let {
         it.layout(::setWidth, ::setHeight)
         setModifiers(it.build())
